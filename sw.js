5aae121a5a9363681ab7359a51c24563bf353f51
Karan <pspokestar@gmail.com>
2026-03-23 12:40:02 +0530

    Update CACHE_VERSION to 'op-tracker-v16'
    Bump cache version to ensure old cache is pruned.

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * so the old cache is pruned automatically on the next visit.
  */
 
-const CACHE_VERSION  = 'op-tracker-v15';
+const CACHE_VERSION  = 'op-tracker-v16';
 const CACHE_NAME     = CACHE_VERSION;
 
 /**
