71974a6deb9645af491f5fcf48e226ad9baadac1
Karan <pspokestar@gmail.com>
2026-04-05 20:25:31 +0530

    Update CACHE_VERSION to 'op-tracker-v37'
    Bump cache version to ensure old cache is pruned.

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * so the old cache is pruned automatically on the next visit.
  */
 
-const CACHE_VERSION  = 'op-tracker-v36';
+const CACHE_VERSION  = 'op-tracker-v37';
 const CACHE_NAME     = CACHE_VERSION;
 
 /**
